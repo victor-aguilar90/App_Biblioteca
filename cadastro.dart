505e082758d31c472164a37e7a4e8a2d5875873b
victor-aguilar90 <143664321+victor-aguilar90@users.noreply.github.com>
2024-05-24 15:12:37 -0300

    Update cadastro.dart

--- cadastro.dart
+++ cadastro.dart
@@ -2,43 +2,98 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main(){
-  runApp(PaginaP());
+  runApp(SignUp());
 }
 
-class PaginaP extends StatelessWidget {
-  const PaginaP({super.key});
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Pagina(),
+    return MaterialApp(
+      home: cadastro(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class Pagina extends StatefulWidget {
-  const Pagina({super.key});
+class cadastro extends StatefulWidget {
+  const cadastro({super.key});
 
   @override
-  State<Pagina> createState() => _PaginaState();
+  State<cadastro> createState() => _cadastroState();
 }
 
-class _PaginaState extends State<Pagina> {
+class _cadastroState extends State<cadastro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
-      appBar: AppBar(
-        title: Text("Biblioteca Ohara", style: GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.w500, shadows: [Shadow(blurRadius: 3.0, color: Colors.black.withOpacity(0.25), offset: Offset(0, 4))]),),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home, color: Colors.white,), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-          BottomNavigationBarItem(icon: Icon(Icons.book),label: "Books")
-        ],
+      body: Center(
+        child: Container(
+          width: 341,
+          height: 642,
+          child: Column(
+            children: [
+              Container(
+                width: 341,
+                child: Text("Cadastre-se", style: GoogleFonts.poppins(fontSize: 40, fontWeight: FontWeight.w500), textAlign: TextAlign.left,),
+              ),
+              SizedBox(height: 46,),
+              TextField(
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  labelText: "Nome completo",
+                  labelStyle: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.black),
+                  hintText: "Digite seu nome completo",
+                ),
+              ),
+              SizedBox(height: 31,),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Endereço de Email",
+                  labelStyle: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.black),
+                  hintText: "Digite seu email",
+                  hintStyle: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w300)
+                ),
+              ),
+              SizedBox(height: 31,),
+              TextField(
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.black),
+                  hintText: "Digite uma senha",
+                  hintStyle: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w300)
+                ),
+              ),
+              SizedBox(height: 31,),
+              TextField(
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: "Confirme sua senha",
+                  labelStyle: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w400, color: Colors.black),
+                  hintText: "Digite novamente sua senha",
+                  hintStyle: GoogleFonts.poppins(fontSize: 15, fontWeight: FontWeight.w300)
+                ),
+              ),
+              SizedBox(height: 46,),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(341, 57),
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                  )
+                ), 
+                child: Text("Criar Conta", style: GoogleFonts.poppins(fontSize: 26, fontWeight: FontWeight.w600, color: Colors.white),)
+              )
+            ],
+          ),
+        ),
       ),
     );
   }

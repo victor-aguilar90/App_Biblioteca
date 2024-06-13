50ac0d453c72e148a3eef596cb486c360b1be234
victor-aguilar90 <143664321+victor-aguilar90@users.noreply.github.com>
2024-06-13 15:19:43 -0300

    Update Aula - Atualizado.sql

--- Aula - Atualizado.sql
+++ Aula - Atualizado.sql
@@ -14,10 +14,31 @@ create table Professor(
     sal_prof decimal(10,2)
 );
 
+create table Cargo (
+	cod_cargo int auto_increment primary key,
+    nome_cargo varchar(120) unique
+);
+
 create table Aluno(
 	cod_aluno int auto_increment primary key,
     nome_aluno varchar(40),
     sexo_aluno char(1)
+);
+
+create table Funcionario(
+	cod_func int auto_increment primary key,
+    cod_cargo int,
+    nome_func varchar(120),
+    salario_func decimal(10,2),
+    rg_func varchar(120),
+    foreign key (cod_cargo) references Cargo(cod_cargo)
+);
+
+create table Dependente(
+	cod_dep int auto_increment primary key,
+    cod_func int,
+    nome_dep varchar(120),
+    foreign key (cod_func) references Funcionario(cod_func)
 );
 
 insert into Cliente(nome_cli, sexo_cli, sal_cli) values
@@ -39,3 +60,4 @@ insert into Aluno(nome_aluno, sexo_aluno) values
 ("Luis Nunes", "M"),
 ("Sandra Silvia", "F"),
 ("Celina Lima", "F");
+

fa03391aeec6914004ea8697784b978a156cb0a9
victor-aguilar90 <143664321+victor-aguilar90@users.noreply.github.com>
2024-06-19 10:27:29 -0300

    Update biblioteca.dart

--- biblioteca.dart
+++ biblioteca.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main(){
   runApp(PaginaP());
@@ -29,35 +28,171 @@ class _PaginaState extends State<Pagina> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
-      appBar: AppBar(
-        title: Text("Biblioteca Ohara", style: GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.w500, shadows: [Shadow(blurRadius: 3.0, color: Colors.black.withOpacity(0.25), offset: Offset(0, 4))]),),
-        toolbarHeight: 70,
+      backgroundColor: Colors.white,
+      drawer: Drawer(
+
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                width: 381,
-                height: 69,
-                child: TextField(
-                decoration: InputDecoration(
-                  
-                  hintText: "Pesquisar livros...",
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20)
-                  )
-                ),
+      appBar: AppBar(
+
+        title: Text("Biblioteca Ohara", style: GoogleFonts.poppins(fontSize: 32, fontWeight: FontWeight.w500,),),
+        toolbarHeight: 80,
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(left: 35, top: 25),
+        scrollDirection: Axis.vertical,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 380,
+              child: Text("Livros", style: GoogleFonts.poppins(fontSize: 30), textAlign: TextAlign.left,),
+            ),
+            SizedBox(height: 15,),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 235,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                ],
               ),
-            )
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 380,
+              child: Text("Terror", style: GoogleFonts.poppins(fontSize: 30), textAlign: TextAlign.left,),
+            ),
+            SizedBox(height: 12,),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 235,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: 380,
+              child: Text("Drama", style: GoogleFonts.poppins(fontSize: 30), textAlign: TextAlign.left,),
+            ),
+            SizedBox(height: 12,),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 232,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                  Container(
+                    width: 172,
+                    height: 235,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  SizedBox(width: 17,),
+                ],
+              ),
+            ),
           ],
         )
-        ],
-       )
+      )
       );
     }
 }
